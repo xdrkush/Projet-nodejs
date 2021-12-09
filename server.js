8c9af4e3c96c4654d433758231ed33e9569b82e3
Axel <axel.etoile.espoire@gmail.com>
2021-12-09 11:31:47 +0100

    Init

---
+++ server.js
@@ -0,0 +1,28 @@
+const
+    express = require('express'),
+    { engine } = require('express-handlebars'),
+    env = require('dotenv').config(),
+    app = express();
+
+//
+app.set('view engine', 'hbs')
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
+
+// Route par défaut
+app.get('/', function (req, res) {
+    res.render('home')
+}) 
+// Route Bruno
+app.get('/bruno', function (req, res) {
+    res.send('<h2>Team Bruno</h2>>')
+})
+// Route Admin
+app.get('/admin', function (req, res) {
+    res.redirect('/')
+})
+
+// Lancement de l'app sur le port défini dans le .env
+app.listen(process.env.PORT)
