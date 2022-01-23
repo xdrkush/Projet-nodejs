646d8506d20bc7a3cb7a26041d26901b63e48ac9
Axel <axel.etoile.espoire@gmail.com>
2022-01-23 14:43:43 +0000

    Add back to top & loader

--- public/js/index.js
+++ public/js/index.js
@@ -150,5 +150,30 @@
       mirror: false
     })
   });
+  /**
+   * Preloader
+   */
+   let preloader = select('#preloader');
+   if (preloader) {
+     window.addEventListener('load', () => {
+       preloader.remove()
+     });
+   }
 
+  /**
+   * Back to top button
+   */
+   let backtotop = select('.back-to-top')
+   if (backtotop) {
+     const toggleBacktotop = () => {
+       if (window.scrollY > 100) {
+         backtotop.classList.add('active')
+       } else {
+         backtotop.classList.remove('active')
+       }
+     }
+     window.addEventListener('load', toggleBacktotop)
+     onscroll(document, toggleBacktotop)
+   }
+   
 })()
